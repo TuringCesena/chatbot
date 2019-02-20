5c3df2b144c7bcf642c1a5787f9738ae20a2f77a
Federico Garcea <federico@tecknowcore.com>
2019-02-20 12:12:32 +0100

    Aggiunta di Cors Policy e Swagger, cambio di host a Azure.

--- api/ChatbotApi/Startup.cs
+++ api/ChatbotApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ChatbotApi
 {
@@ -23,10 +24,33 @@ namespace ChatbotApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    });
+            });
             services.AddMvc();
+            
+            services.AddSwaggerGen(c =>
+            {
+                    c.IncludeXmlComments(GetXmlCommentsPath());
+            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configure base services we are going to use (MVC and Swagger)
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,7 +58,22 @@ namespace ChatbotApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors("AllowAll");
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseMvc();
+            
         }
+
+        private string GetXmlCommentsPath()
+        {
+            var app = System.AppContext.BaseDirectory;
+            return System.IO.Path.Combine(app, "ChatBotApi.xml");
+        }
+
     }
 }
